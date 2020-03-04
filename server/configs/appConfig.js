5826f83aaaa54f8d4479d078a0b2bba95a0c9b2d
Alexandr Kudryashov <kudryashov_as@astral.ru>
2020-03-04 10:20:54 +0300

    add npm scripts

--- server/configs/appConfig.js
+++ server/configs/appConfig.js
@@ -1,17 +1,16 @@
-/*main app config*/
+/* main app config */
 const dev = process.env.NODE_ENV !== "production";
-const clientPort = dev?4000:4001;
-const serverPort = dev?5000:5001;
+const clientPort = dev ? 4000 : 4001;
+const serverPort = dev ? 5000 : 5001;
 
 const dbConfig = {
-    uri:"mongodb://127.0.0.1:27017/",
-    dbName:"Restorest"
-}
+  uri: "mongodb://127.0.0.1:27017/",
+  dbName: "Restorest"
+};
 
 export const APP_CONFIG = {
-    mode:dev,
-    clientPort,
-    serverPort,
-    dbConfig
-}
-
+  mode: dev,
+  clientPort,
+  serverPort,
+  dbConfig
+};

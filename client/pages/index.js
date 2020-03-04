0824951bf49245b404b8919df1cf5a929fc52b49
Alexandr Kudryashov <kudryashov_as@astral.ru>
2020-03-04 10:31:47 +0300

    simple form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,27 @@
+import {useState} from "react";
+
 const Index = () => {
-  return <div>test</div>;
+  const [data, setData] = useState({ login: "", password: "" });
+
+  const handleChangedata = name => e => {
+    setData({ ...data, [name]: e.target.value });
+  }
+
+  const handleLogin = e => {
+    e.preventDefault();
+    console.log(data);
+  }
+
+  return (
+    <form>
+      <input type="text" name="login" onChange={handleChangedata("login")} />
+      <br />
+      <br />
+      <input type="password" name="password" onChange={handleChangedata("password")} />
+      <br />
+      <button onClick={handleLogin}>login</button>
+    </form>
+  );
 }
 
 export default Index;
